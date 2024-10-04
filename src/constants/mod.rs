7a1bfbae74f40c0d8bf9fce741dbb29568b1fb67
Merciless <yurii.afterglow@gmail.com>
2024-10-04 10:06:34 +0300

    fix bugs

--- src/constants/mod.rs
+++ src/constants/mod.rs
@@ -2,7 +2,7 @@ pub const MODE_COMMON: &str = "--mode common";
 pub const MODE_POLISH: &str = "--mode polish";
 pub const EXIT_COMMAND: &str = "q";
 
-pub const MEMORY_FLAG: &str = "-R";
+pub const MEMORY_FLAG: &str = "R";
 
 #[derive(Debug)]
 pub enum Mode {
